8d58adcec56351e0e29123203cc0723b5cec87ef
Júlia Forny de Souza Muniz <forny918@gmail.com>
2026-03-16 09:48:38 -0300

    feat: add movies and sessions endpoints

--- movies/urls.py
+++ movies/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
-urlpatterns = []
+from .views import MovieListView, MovieDetailView, SessionListView, SessionDetailView
+
+urlpatterns = [
+    path('', MovieListView.as_view(), name='movie-list'),
+    path('<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
+    path('<int:movie_id>/sessions/', SessionListView.as_view(), name='session-list'),
+    path('sessions/<int:pk>/', SessionDetailView.as_view(), name='session-detail'),
+]
